a074cecdf110cc4d80d6f7c0ee2eda6f47591420
Hacker-samy <126173507+Hacker-samy@users.noreply.github.com>
2023-02-24 23:25:11 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+clear
+apt update -y
+apt upgrade -y
+pkg intall php -y
+pkg inatall curl -y
+clear
+sleep 1
+xdg-open https://www.youtube.com/channel/HYDRAGAMING4U
+echo " wait for 15 seconds "
+sleep 15
+echo " create account on cashkaro.com "
+echo " set your browser "
+termux-open-url https://cashkaro.com?r=8026255&fname=Rixon
+echo ""
